b2cc2004ba23d1ad5365c376ad47ac0459f5b7d6
KaWeco258 <138412692+KaWeco258@users.noreply.github.com>
2024-04-24 13:20:06 +0900

    実行可能状態

--- Development/Objects/Player/Player.cpp
+++ Development/Objects/Player/Player.cpp
@@ -33,7 +33,7 @@ void Player::Initialize()
 	radian = 0.0f;
 
 	//当たり判定の大きさを設定
-	box_size = Vector2D(64.0f);
+	scale = 64.0f;
 
 	//初期画像の設定
 	image = animation[0];
@@ -57,8 +57,8 @@ void Player::Draw() const
 	//デバッグ用
 #if _DEBUG
 	//当たり判定の可視化
-	Vector2D ul = location - (box_size / 2.0f);
-	Vector2D br = location + (box_size / 2.0f);
+	Vector2D ul = location - (scale / 2.0f);
+	Vector2D br = location + (scale / 2.0f);
 
 	DrawBoxAA(ul.x, ul.y, br.x, br.y, GetColor(255, 0, 0), FALSE);
 #endif	   
